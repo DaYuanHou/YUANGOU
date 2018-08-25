fc96301335340e65b3cd0ca3a403a54ac59093e1
DaYuanHou <psy1226@yahoo.com>
2018-08-25 19:13:42 +0800

    修改测试类

--- yuangou-dao/src/test/java/DaoTest.java
+++ yuangou-dao/src/test/java/DaoTest.java
@@ -1,10 +1,13 @@
 
+import com.github.pagehelper.Page;
+import com.github.pagehelper.PageHelper;
 import com.yuangou.mapper.TbBrandMapper;
 import com.yuangou.pojo.TbBrand;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.transaction.annotation.Transactional;
 
 import javax.annotation.Resource;
 import java.util.List;
@@ -24,12 +27,25 @@ import java.util.List;
 @ContextConfiguration(locations = {"classpath:spring/applicationContext-dao.xml"})
 public class DaoTest {
     @Resource
-    private TbBrandMapper tbBrandMapper;
+    private TbBrandMapper tBrandMapper;
+
+//    @Test
+//    @Transactional
+//    public void dss() {
+//        TbBrand t = new TbBrand();
+//      t.setFirstChar("v");
+//        t.setId(10l);
+//        t.setName("V");
+//
+//        tBrandMapper.updateByPrimaryKey(t);
+//
+//    }
 
     @Test
-    public void dss() {
-        List<TbBrand> list = tbBrandMapper.selectByExample(null);
-        System.out.println(list.size());
-
+    public void ss() {
+        PageHelper.startPage(1, 10);
+        Page<TbBrand> pages = (Page<TbBrand>) tBrandMapper.selectByExample(null);
+        System.out.println(pages.getTotal());
+        System.out.println(pages.getResult().get(0).getName());
     }
 }
