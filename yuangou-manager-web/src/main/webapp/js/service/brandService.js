1d842d7b2268b1effccfba33614aa8a83ba4d2c9
DaYuanHou <psy1226@yahoo.com>
2018-08-25 22:46:30 +0800

    增加service

---
+++ yuangou-manager-web/src/main/webapp/js/service/brandService.js
@@ -0,0 +1,32 @@
+app.service('brandService', function ($http) {
+    this.findAll = function () {
+        return $http.get('../brand/findAll.do');
+    }
+    this.findPage = function (page, rows) {
+        return $http.get('../brand/findPage.do?page=' + page + '&size=' + rows);
+    }
+
+    this.findOne = function (id) {
+        return $http({
+            method: "POST",
+            url: "../brand/findOne.do",
+            params: {
+                id: id
+            }
+        });
+    }
+
+    this.add = function (entity) {
+        return $http.post("../brand/add.do", entity);
+    };
+    this.update = function (entity) {
+        return $http.post("../brand/update.do", entity);
+
+    };
+    this.dele = function (ids) {
+        return $http.get('../brand/delete.do?ids=' + ids);
+    };
+    this.search = function (page, rows, entity) {
+        return $http.post('../brand/search.do?page=' + page + "&rows=" + rows, entity);
+    }
+});
